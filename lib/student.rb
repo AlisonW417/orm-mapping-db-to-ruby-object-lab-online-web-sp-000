1706bc44ba5758887f7a7718e56630d4aa2d1f56
Alison Williams <apw5011@gmail.com>
2021-01-16 20:55:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,10 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    student = self.new(id, name, grade)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
     student 
   end
 
